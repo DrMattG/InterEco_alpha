a7c1d0d761a188f018a210df2adc950710e3d236
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-09-27 15:57:14 +0200

    streamline the R6 class again as path not working

--- R/appData.R
+++ R/appData.R
@@ -9,19 +9,17 @@ AppClass=R6::R6Class(classname = "AppData",
                        pred= NULL,
                        #' @field modx The name of the moderator variable involved in the interaction
                        modx= NULL,
-                       #' @field path filepath to summary Rmd file 
-                       sum_path=NULL,
+                       
                        #' @description
                        #' Create a new object.
                        #' @param data dataframe
                        #' @param model model object
                        #' @return A new `AppD` object.
-                       initialize = function(data = NA, model = NA, pred=NA, modx=NA, sum_path=NA) {
+                       initialize = function(data = NA, model = NA, pred=NA, modx=NA) {
                          self$data <- data
                          self$model <- model
                          self$pred<-pred
                          self$modx<-modx
-                         self$sum_path<-sum_path
                        }
                      )
                      
@@ -32,17 +30,16 @@ AppClass=R6::R6Class(classname = "AppData",
 #' @param model the lm model object (limited to lm for this example)
 #' @param pred The name of the predictor variable involved in the interaction.
 #' @param modx The name of the moderator variable involved in the interaction.
-#' @return an R6 class object with both model and data together (use $ to call each e.g. 'appdata$model')
+#' @return an R6 class object with both model and data together (use slots to call each e.g. 'appdata$model')
 #' @export
 #'
 #' @examples
 #' appdata <- makeAppData(data = iris, model = fitiris, pred="Petal.width", modx="Species")
-makeAppData <- function(data = data, model = model, pred=pred, modx=modx, sum_path=sum_path) {
+makeAppData <- function(data = data, model = model, pred=pred, modx=modx) {
     appdata <- AppClass$new(data = data,
                  model = model,
                  pred=pred,
-                 modx=modx,
-                 sum_path=sum_path
+                 modx=modx
   )
 }
 
