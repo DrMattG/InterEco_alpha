c9349f1c8547f8b7ae2776e3e87354a7200cc13c
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-09-27 13:34:57 +0200

    Change to R6 class for appData

--- R/appData.R
+++ R/appData.R
@@ -1,23 +1,42 @@
 # set up a new class for model objects and data
-
-methods::setClass("appData", slots = list(data = "data.frame", model = "lm", pred="character",modx="character"))
-
-# make the lm model with an interaction
-fitiris <- lm(Petal.Length ~ Petal.Width * Species, data = iris)
+AppClass=R6::R6Class(classname = "AppData", 
+                     public = list(
+                       #' @field data the dataframe that the model is generated from
+                       data = NULL,
+                       #' @field model the model object
+                       model = NULL,
+                       #' @field pred The name of the predictor variable involved in the interaction.
+                       pred= NULL,
+                       #' @field modx The name of the moderator variable involved in the interaction
+                       modx= NULL,
+                       
+                       #' @description
+                       #' Create a new object.
+                       #' @param data dataframe
+                       #' @param model model object
+                       #' @return A new `AppD` object.
+                       initialize = function(data = NA, model = NA, pred=NA, modx=NA) {
+                         self$data <- data
+                         self$model <- model
+                         self$pred<-pred
+                         self$modx<-modx
+                       }
+                     )
+                     
+)
 
 #' make data for the app
 #' @param data the dataframe that the model is generated from
 #' @param model the lm model object (limited to lm for this example)
 #' @param pred The name of the predictor variable involved in the interaction.
 #' @param modx The name of the moderator variable involved in the interaction.
-#' @return an S4 class object with both model and data together (use slots to call each e.g. 'appdata@model')
+#' @return an R6 class object with both model and data together (use $ to call each e.g. 'appdata$model')
 #' @export
 #'
 #' @examples
-#' appdata <- makeAppData(data = iris, model = fitiris, pred=Petal.width, modx=Species)
+#' appdata <- makeAppData(data = iris, model = fitiris, pred="Petal.width", modx="Species")
 makeAppData <- function(data = data, model = model, pred=pred, modx=modx) {
-  appdata <- new("appData",
-                 data = data,
+    appdata <- AppClass$new(data = data,
                  model = model,
                  pred=pred,
                  modx=modx
