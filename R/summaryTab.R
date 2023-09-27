9e4394b5916bd754335dd4fe59903999665451ff
Simon Rolph <simon.p.rolph@gmail.com>
2023-09-27 11:58:41 +0100

    Sumamry tab function that loads from file and renders an rmd

--- R/summaryTab.R
+++ R/summaryTab.R
@@ -1,15 +1,12 @@
 #function to create the summary page
 summaryTab <- function(appData,filePath){
   
+  # Render the Rmd to HTML
+  html_file <- rmarkdown::render(filePath,output_file = tempfile())
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  shiny::tabPanel("Study Summary", shiny::includeHTML(html_file))
 }
+
+#testing
+#generateTemplateSummary("test.Rmd")
+#summaryTab(NA,"test.Rmd")
