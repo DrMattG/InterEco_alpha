c7bfa403112e30c79843d74aeecdb0ad3cb0c844
Diana Bowler <diana.e.bowler@gmail.com>
2023-10-13 11:58:48 +0100

    plot to explore covariate space (highlighting location of median values)

---
+++ R/covariatePlots.R
@@ -0,0 +1,69 @@
+# Plotting functions to visualize the covariate space and assess the location of median and mean values
+
+#' @param C character string of all model predictors
+#' @param data dataframe
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#'  appdata <- makeAppData(data = mtcars, model = fiti, pred="hp", modx="wt")
+#'  covariatespacePlot(D = appdata$pred, X = appdata$modx, data = appdata$data)
+
+#for ants C is "lt_clim","woody","perc_clay","bare","lui","exotic_grnd"
+
+covariatespacePlot<-function(C, data){
+  
+  #get covariate data frame
+  covs <- data[,C]
+  covs <- covs[complete.cases(covs),]
+  
+  #functions for the ggpairs plot
+  upper_fn <- function(data, mapping, ...){
+    p <- ggplot(data = data, mapping = mapping) + 
+      stat_density2d(aes(fill=..density..), geom="tile", contour = FALSE) +
+      scale_fill_gradientn(colours=terrain.colors(100)) +
+      geom_point(size=0.5, alpha = 0.5)
+    p
+  }
+  
+  diag_fn <- function(data, mapping, ...){
+    p <- ggplot(data = data, mapping = mapping) + 
+      geom_histogram() +
+      theme_bw()
+    p
+  }
+  
+  #get the basic plot
+  m <- ggpairs(covs, 
+               lower = list(continuous = upper_fn),
+               diag = list(continuous = diag_fn),
+               upper = 'blank',
+               progress  =FALSE)
+  
+  #modify the off-diags to add on the median covariate value locations
+  for (i in 2:m$nrow) {
+    for (j in 1:(i-1)) {
+      p <- getPlot(m, i, j)
+      m[i,j] = m[i,j] + geom_point(x = median(covs[,p$labels$x][[1]], na.rm=T),
+                                   y = median(covs[,p$labels$y][[1]], na.rm=T),
+                                   col = "red",
+                                   size = 3)
+    }
+  }
+  
+  #modify the diags with lines to higlight the location of the median values further
+  for(i in 1:m$nrow){
+    p <- getPlot(m, i, i)
+    
+    m[i,i] <- m[i,i] + geom_vline(xintercept = median(covs[,p$labels$x][[1]], na.rm=T),
+                                  col = "red",
+                                  linetype = "dotted",
+                                  linewidth = 1.5)
+  }
+
+}
+
+
+
