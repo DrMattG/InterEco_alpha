d685c6eeebfd5d26d5ba532c02ae8198eaf7b1a2
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2024-04-26 09:48:17 +0100

    distance metric not needed

--- R/paramSpacePlot.R
+++ R/paramSpacePlot.R
@@ -10,26 +10,22 @@ plot_param_space_scatter <- function(model, data) {
   
   vars <- all.vars(formula(model))
   
-  scaled_covs <- data |>
+  covs <- data |>
     dplyr::select(vars) |>
-    tidyr::drop_na() |>
-    dplyr::mutate_all(scale)
-    
-  scaled_covs$md <- mahalanobis(scaled_covs,
-                                colMeans(scaled_covs),
-                                cov(scaled_covs)) 
-    
+    tidyr::drop_na()
   
   m <- GGally::ggpairs(covs, 
                lower = list(continuous = upper_fn),
                diag = list(continuous = diag_fn),
                upper = "blank",
-               progress  =FALSE)
+               progress = FALSE)
   
+  # add points at median for density plots
   for (i in 2:m$nrow) {
     for (j in 1:(i-1)) {
       p <- GGally::getPlot(m, i, j)
-      m[i,j] = m[i,j] + 
+      
+      m[i,j] <- m[i,j] + 
         annotate(geom = "point", x = median(covs[,p$labels$x][[1]], 
                               na.rm = TRUE),
                                    y = median(covs[,p$labels$y][[1]], 
@@ -39,6 +35,7 @@ plot_param_space_scatter <- function(model, data) {
     }
   }
   
+  # add dotted line at median for histograms
   for(i in 1:m$nrow){
     p <- GGally::getPlot(m, i, i)
     
@@ -61,7 +58,8 @@ upper_fn <- function(data, mapping, ...) {
     stat_density2d(aes(fill = after_stat(density)), 
                    geom = "tile", contour = FALSE) +
     scale_fill_gradientn(colours = terrain.colors(100)) +
-    geom_point(size = 0.5, alpha = 0.6)
+    geom_point(size = 0.5, alpha = 0.6) +
+    theme_classic()
   p
 }
 
