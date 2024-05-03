7a96373d4f326ebe6e70b7695d180f024faddc8c
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2024-05-03 09:58:26 +0100

    actually plot the resids  and omit NAs

--- R/residualPlots.R
+++ R/residualPlots.R
@@ -3,7 +3,8 @@
 #' @import ggplot2
 #' @import DHARMa
 #' @importFrom dplyr mutate
-#' @return plot of redsiduals
+#' @importFrom tidyselect any_of where
+#' @return plot of residuals
 #' @export
 
 plot_resids <- function(model, data){
@@ -11,15 +12,18 @@ plot_resids <- function(model, data){
   resDHARMa <- DHARMa::simulateResiduals(model, plot = FALSE, 
                                          integerResponse = T)
   
-  p <- data |> dplyr::mutate(
+  p <- data |> 
+    dplyr::select(all.vars(formula(model))) |>
+    na.omit() |>
+    dplyr::mutate(
     resids = resDHARMa$scaledResiduals,
-    predicted = resDHARMa$fittedPredictedResponse,
-    observed = resDHARMa$observedResponse
+    predicted = resDHARMa$fittedPredictedResponse
   ) |>
-    dplyr::select(all.vars(formula(model))[-1]) |>
-    tidyr::pivot_longer(cols = all.vars(formula(appdata$model))[-1],
+    dplyr::select(-all.vars(formula(model))[1]) |>
+    dplyr::select_if(tidyselect::where(is.numeric)) |>
+    tidyr::pivot_longer(cols = tidyselect::any_of(all.vars(formula(appdata$model))[-1]),
                         names_to = "vars") |>
-    ggplot2::ggplot(aes(x = vars, y = value)) +
+    ggplot2::ggplot(aes(x = value, y = resids)) +
     ggplot2::geom_jitter(
       width = 0.1,
       shape = 16,
